7692ce822862dfcee54bcefcad1ed625285162b7
Jacder <coderoj@outlook.com>
2020-09-26 11:40:02 +0800

    add(P3806)

---
+++ tmp/P3806/main.cpp
@@ -0,0 +1,293 @@
+#include <iostream>
+#include <cstdio>
+using namespace std; 
+#ifndef _LIB_SCANNER
+#define _LIB_SCANNER 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+class Scanner {
+private:
+    static const int BUFFER_SIZE = 10000;
+    char buff[BUFFER_SIZE];
+    int buffPos, buffLim;
+
+public:
+    Scanner () { 
+        buffLim = fread(buff, 1, BUFFER_SIZE, stdin);
+        buffPos = 0;
+    }
+
+private:
+    inline void flushBuff() {
+        buffLim = fread(buff, 1, BUFFER_SIZE, stdin);
+        if (buffLim==0) {
+            buff[buffLim++] = '\n';
+        }
+        buffPos = 0; 
+    }
+
+    inline bool isWS(char t) {
+        return t==' ' || t=='\n';
+    }
+
+    inline bool isDig(char t) {
+        return t>='0' && t<='9'; 
+    }
+
+    void nextPos() {
+        buffPos++;
+        if (buffPos == buffLim) {
+            flushBuff();
+        }
+    }
+
+#define getChar() buff[buffPos]
+
+public:
+    inline char getchar() {
+        char ch=getChar();
+        nextPos();
+        return ch; 
+    }
+
+    inline void next(char* s) {
+        while ( isWS(getChar()) ) {
+            nextPos(); 
+        }
+        while ( !isWS(getChar()) ) {
+            *s = getChar(); s++;
+            nextPos();
+        }
+        *s = '\0';
+    }
+
+    inline void nextLine(char* s) {
+        while ( getChar()!='\n' ) {
+            nextPos();
+        }
+        if ( getChar()=='\n' ) {
+            nextPos();
+        }
+        while ( getChar() != '\n' ) {
+            *s = getChar(); s++;
+            buffPos++;
+        }
+        *s = '\0';
+    }
+
+    inline int nextInt() {
+        while ( !isDig(getChar()) ) {
+            nextPos();
+        }
+        int sign = (getChar() == '-') ? nextPos(),-1 : 1;
+        int res = 0;
+        while (isDig(getChar())) {
+            res = res*10 + getChar()-'0';
+            nextPos();
+        }
+        return res * sign; 
+    }
+
+    inline double nextDouble() {
+        while ( isWS(getChar()) ) {
+            nextPos();
+        }
+        int sign = (getChar() == '-') ? nextPos(),-1 : 1;
+        double res = 0;
+        while (isDig(getChar())) {
+            res = res * 10 + getChar()-'0';
+            nextPos();
+        }
+        if (getChar() == '.') {
+            nextPos();
+            double ep = 1;
+            while (isDig(getChar())) {
+                ep *= 0.1;
+                res += ep * (getChar()-'0');
+                nextPos();
+            }
+        }
+        return sign * res;
+    }
+
+    inline char nextChar() {
+        while (isWS(getChar())) nextPos();
+        char res = getChar(); nextPos();
+        return res;
+    }
+
+#undef getChar
+};
+Scanner sc;
+#endif /* _LIB_SCANNER */
+template <int N, class T, T Zero> class Array {
+protected:
+    T a[N]; int upt[N];
+    int t;
+
+public:
+    Array () {
+        t=0; for (int i=0;i<N;i++) upt[i]=0; }
+    T& operator [] (const int i) {
+        if (upt[i] < t) {
+            a[i] = Zero; upt[i] = t; }
+        return a[i]; }
+    void clear() {
+        t++; }
+};
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <queue>
+#include <cmath>
+#include <cstring>
+#include <bitset>
+#include <sstream>
+
+#ifdef ENABLE_LL
+#define int long long
+#define I "%lld"
+#define II "%lld%lld"
+#define III "%lld%lld%lld"
+#define IIII "%lld%lld%lld%lld"
+#define IN "%lld\n"
+#define IIN "%lld%lld\n"
+#define IIIN "%lld%lld%lld\n"
+#define IIIIN "%lld%lld%lld%lld\n"
+#else
+#define I "%d"
+#define II "%d%d"
+#define III "%d%d%d"
+#define IIII "%d%d%d%d"
+#define IN "%d\n"
+#define IIN "%d%d\n"
+#define IIIN "%d%d%d\n"
+#define IIIIN "%d%d%d%d\n"
+#endif
+#define repe(a,b) for (int a=0;a<(int)b.size();a++)
+#define rep(i,n) for (int i=0;i<n;i++)
+#define repa(i,n) for (int i=1;i<=n;i++)
+#define repi(i,a,b) for (int i=a;i<=b;i++) 
+#define repb(i,a,b) for (int i=a;i>=b;i--)
+
+typedef pair < int , int > pr; 
+typedef pair < pr  , int > prr; 
+template <class T> inline bool checkMin(T& a , T b) { return (a > b ? a=b,1 : 0); } 
+template <class T> inline bool checkMax(T& a , T b) { return (a < b ? a=b,1 : 0); } 
+void ALERT(bool judgememt, const char* phrase) { if (judgememt) { puts(phrase); throw "ALERT"; } } 
+bool alert(bool judgememt, const char* phrase) { if (judgememt) puts(phrase); return judgememt; }
+#define L first
+#define R second
+
+#ifdef __LOCALE__
+#define see(a) std::cout << #a << "=" << a << std::endl
+#define ses(a) std::cout << #a << "=" << a << " " 
+#else
+#define see(a) //std::cout << #a << "=" << a << std::endl
+#define ses(a)
+#endif
+
+#ifndef CUSTOM_MAIN
+
+void preInit();
+void init(); 
+void solve(); 
+
+int32_t main(){ 
+preInit();
+
+#ifdef MULTIPLE_TEST_CASES_WITH_T
+int T; 
+
+#ifdef _LIB_SCANNER
+T = sc.nextInt();
+#else
+scanf("%d",&T); 
+#endif /* _LIB_SCANNER */
+
+while(T--) {init(); solve();} 
+#else
+
+#ifdef MULTIPLE_TEST_CASES_WITHOUT_T 
+while(1) {
+    try {init(); } 
+    catch(bool t) {return 0;} 
+    solve();
+}
+#else
+init(); solve(); 
+#endif /* MULTIPLE_TEST_CASES_WITHOUT_T  */
+
+#endif /* MULTIPLE_TEST_CASES_WITH_T */
+    return 0;
+}
+
+#endif /* CUSTOM_MAIN */
+
+// my code begins here
+
+const int N = 10005;
+const int M = 10000005;
+vector <pr> e[N];
+vector <int> roots[N];
+int n,m,k;
+
+void preInit() {
+    n = sc.nextInt(); m = sc.nextInt();
+    rep (i,n-1) {
+        int u=sc.nextInt(), v=sc.nextInt(), w=sc.nextInt();
+        e[u].push_back({v,w}); e[v].push_back({u,w}); }
+    e[0].push_back({1,20050610});
+}
+
+void init() {
+     
+}
+
+int vis[N], sz[N];
+int tot,root,ans;
+
+void getSize(int u, int f) {
+    sz[u] = 1;
+    for (pr v: e[u]) if (v.L!=f && !vis[v.L]) {
+        getSize(v.L,u); sz[u] += sz[v.L]; } }
+void getRoot(int u, int f) {
+    int maxSub = tot - sz[u];
+    for (pr v: e[u]) if (v.L != f && !vis[v.L]) {
+        getRoot(v.L,u); checkMax(maxSub,sz[v.L]); }
+    if (maxSub*2 <= tot) root=u; }
+
+Array <M,int,0> cnt; int partCnt[N]; int partCntCnt;
+void getCnt(int u, int f, int uw) {
+    if (uw > k) { return; }
+    ans += cnt[k-uw]; partCnt[partCntCnt++] = uw;
+    for (pr v:e[u]) if (v.L != f && !vis[v.L]) {
+        getCnt(v.L, u, v.R+uw); } }
+
+void dfs_pre(int u) {
+    vis[u] = 1; 
+    for (pr v: e[u]) if (!vis[v.L]) {
+        getSize(v.L,v.L); tot=sz[v.L]; getRoot(v.L,v.L);
+        roots[u].push_back(root); dfs_pre(root); } }
+
+void dfs(int u) {
+    vis[u] = 1; 
+    cnt.clear(); cnt[0] = 1; partCntCnt=0;
+    for (pr v: e[u]) if (!vis[v.L]) {
+        partCntCnt=0; getCnt(v.L,u,v.R); 
+        rep (i,partCntCnt) { cnt[ partCnt[i] ]++; } }
+    for (int v: roots[u]) {
+        dfs(v); } }
+
+void solve() {
+    dfs_pre(0);
+    rep (i,m) {
+        memset(vis,0,sizeof(vis));
+        k = sc.nextInt(); ans=0;
+        dfs(roots[0][0]);
+        puts(ans ? "AYE" : "NAY"); }
+}
