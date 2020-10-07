6dfcc6d3e3a70529ee50dc3b7e79707c4eb2fb79
Jacder <coderoj@outlook.com>
2020-10-07 19:45:24 +0800

    add(XJOI dev 1570)

---
+++ XJOI/dev_1570/A/std.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <cstdio>
+using namespace std; 
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <queue>
+#include <cmath>
+#include <cstring>
+#include <bitset>
+#include <sstream>
+
+#ifdef ENABLE_LL
+#define int long long
+#define I "%lld"
+#define II "%lld%lld"
+#define III "%lld%lld%lld"
+#define IIII "%lld%lld%lld%lld"
+#define IN "%lld\n"
+#define IIN "%lld%lld\n"
+#define IIIN "%lld%lld%lld\n"
+#define IIIIN "%lld%lld%lld%lld\n"
+#else
+#define I "%d"
+#define II "%d%d"
+#define III "%d%d%d"
+#define IIII "%d%d%d%d"
+#define IN "%d\n"
+#define IIN "%d%d\n"
+#define IIIN "%d%d%d\n"
+#define IIIIN "%d%d%d%d\n"
+#endif
+#define repe(a,b) for (int a=0;a<(int)b.size();a++)
+#define rep(i,n) for (int i=0;i<n;i++)
+#define repa(i,n) for (int i=1;i<=n;i++)
+#define repi(i,a,b) for (int i=a;i<=b;i++) 
+#define repb(i,a,b) for (int i=a;i>=b;i--)
+
+typedef pair < int , int > pr; 
+typedef pair < pr  , int > prr; 
+template <class T> inline bool checkMin(T& a , T b) { return (a > b ? a=b,1 : 0); } 
+template <class T> inline bool checkMax(T& a , T b) { return (a < b ? a=b,1 : 0); } 
+void ALERT(bool judgememt, const char* phrase) { if (judgememt) { puts(phrase); throw "ALERT"; } } 
+bool alert(bool judgememt, const char* phrase) { if (judgememt) puts(phrase); return judgememt; }
+#define L first
+#define R second
+
+#ifdef __LOCALE__
+#define see(a) std::cout << #a << "=" << a << std::endl
+#define ses(a) std::cout << #a << "=" << a << " " 
+#else
+#define see(a) //std::cout << #a << "=" << a << std::endl
+#define ses(a)
+#endif
+
+#ifndef CUSTOM_MAIN
+
+void preInit();
+void init(); 
+void solve(); 
+
+int32_t main(){ 
+preInit();
+
+#ifdef MULTIPLE_TEST_CASES_WITH_T
+int T; 
+
+#ifdef _LIB_SCANNER
+T = sc.nextInt();
+#else
+scanf("%d",&T); 
+#endif /* _LIB_SCANNER */
+
+while(T--) {init(); solve();} 
+#else
+
+#ifdef MULTIPLE_TEST_CASES_WITHOUT_T 
+while(1) {
+    try {init(); } 
+    catch(bool t) {return 0;} 
+    solve();
+}
+#else
+init(); solve(); 
+#endif /* MULTIPLE_TEST_CASES_WITHOUT_T  */
+
+#endif /* MULTIPLE_TEST_CASES_WITH_T */
+    return 0;
+}
+
+#endif /* CUSTOM_MAIN */
+
+// my code begins here
+
+
+
+
+
+const int N = 95;
+int n,m,d;
+vector <pr> e[N];
+
+
+void preInit() { } 
+void init() {
+    scanf(III,&n,&m,&d);
+    rep (i,m) { int u,v,w; scanf(III,&u,&v,&w); e[u].push_back({v,w}); e[v].push_back({u,w}); }
+}
+
+set <int> ans;
+
+void dfs(int u, int state, int step) {
+    if (step == d) { ans.insert(state); return; }
+    for (pr v: e[u]) { dfs(v.L, state*2+v.R, step+1); } }
+
+void solve() {
+    dfs(1, 0, 0);
+    printf(IN,static_cast<int>(ans.size()));
+}
