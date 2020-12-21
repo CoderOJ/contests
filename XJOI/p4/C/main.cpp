8b11359d22302c133c2e0d5c23b3ecd8383ecb23
Jacder <coderoj@outlook.com>
2020-12-21 20:04:49 +0800

    upd(XJOI/p4/C)

--- XJOI/p4/C/main.cpp
+++ XJOI/p4/C/main.cpp
@@ -21,23 +21,15 @@
 
 constexpr int N = 100005;
 
-struct Graph {
-  int n;
-  std::vector<int> e[N];
+struct GraphBase {
+  int n; 
   void init(int _n) { n=_n; }
-  virtual void addEdge(int u, int v) {
-    e[u].push_back(v); e[v].push_back(u); }
-};
+  virtual void addEdge(int u, int v)=0; };
 
-struct GraphDsu : public Graph {
-  int p[N];
-  GraphDsu() { rep (i,N) p[i] = i; }
-  int get(int u) { return u==p[u] ? u : p[u] = get(p[u]); }
-  inline void merge(int u, int v) { p[get(u)] = get(v); }
-  inline bool query(int u, int v) { return get(u) == get(v); } 
-  virtual void addEdge(int u, int v) override final {
-    if (query(u,v)) { return; }
-    merge(u,v); e[u].push_back(v); e[v].push_back(u); }
+struct Graph : GraphBase {
+  std::vector<int> e[N];
+  virtual void addEdge(int u, int v) override {
+    e[u].push_back(v); e[v].push_back(u); }
 };
 
 struct GraphTcc : public Graph {
@@ -58,40 +50,123 @@ struct GraphTcc : public Graph {
         group.pop(); }
       bel[ group.top() ] = this_bel;
       group.pop(); } }
-  void solve(Graph& output) {
+  void solve(GraphBase& output) {
     repa (i,n) if (dfn[i] == -1) { dfs(i,0); }
     output.init(bel_cnt - 1);
-    repa (u,n) for (int v: e[u]) { output.addEdge(bel[u],bel[v]); } }
+    repa (u,n) for (int v: e[u]) { 
+      int bu=bel[u], bv=bel[v];
+      if (bu < bv) { output.addEdge(bu,bv); } } }
 };
 
-struct TreeS : public GraphDsu {
-  static constexpr int B = 20;
-  int fa[N][B], d[N];
+struct TreeLCT : public GraphBase {
+  static constexpr int N = 200005;
+  struct LCT_impl {
+    struct Node {
+      Node *s[2] = {nullptr, nullptr}, *f = nullptr;
+      int val=0, val_sum=0, wei, wei_sum, add=-1, rev=0;
+      Node (int wei=0) : wei(wei), wei_sum(wei) {}
+      bool isRoot() {
+        if (f == nullptr) return 1;
+        return !(f->s[0]==this || f->s[1]==this); }
+      void pushAdd(int del) {
+        val = del*wei; val_sum = del*wei_sum; add = del; }
+      void pushRev() { rev^=1; std::swap(s[0], s[1]); }
+      void pushDown() {
+        if (rev) {
+          if (s[0] != nullptr) { s[0]->pushRev(); }
+          if (s[1] != nullptr) { s[1]->pushRev(); } 
+          rev = 0; }
+        if (add != -1) {
+          if (s[0] != nullptr) { s[0]->pushAdd(add); }
+          if (s[1] != nullptr) { s[1]->pushAdd(add); }
+          add = -1; } }
+      void pushUp() {
+        val_sum = val; wei_sum = wei;
+        if (s[0] != nullptr) { val_sum += s[0]->val_sum; wei_sum += s[0]->wei_sum; }
+        if (s[1] != nullptr) { val_sum += s[1]->val_sum; wei_sum += s[1]->wei_sum; } }
+      void push() {
+        if (!isRoot()) { f->push(); } 
+        pushDown(); }
+      bool getSon() { return f->s[1] == this; }
+      void rotate() {
+        Node *uu=this, *ff=f, *aa=ff->f; bool ss=getSon(); 
+        if (!ff->isRoot()) aa->s[ ff->getSon() ] = uu;
+        ff->f = uu; ff->s[ss] = uu->s[!ss];
+        uu->f = aa; uu->s[!ss] = ff;
+        if (ff->s[ss] != nullptr) { ff->s[ss]->f = ff; }
+        ff->pushUp(); uu->pushUp(); }
+      void _splay() { 
+        if (isRoot()) { return; }
+        if (f->isRoot()) { rotate(); return; } 
+        ( getSon() == f->getSon() ? f : this ) -> rotate(); rotate();
+        _splay(); }
+      Node* splay() { push(); _splay(); return this; }
+      void access() {
+        Node *uu=this, *ss=nullptr; 
+        while (uu!=nullptr) {
+          uu->splay(); uu->s[1]=ss; uu->pushUp();
+          ss = uu; uu = uu->f; } }
+      void makeRoot() { access(); splay(); pushRev(); }
+      Node* findRoot() {
+        access(); splay(); pushDown(); Node* uu=this;
+        while (uu->s[0] != nullptr) { uu = uu->s[0]; uu->pushDown(); } 
+        return uu; }
+    };
 
-  TreeS() { memset(fa, 0, sizeof(fa)); memset(d, 0, sizeof(d)); }
-  void initLca(int u, int f = 0) {
-    d[u] = d[f] + 1; fa[u][0] = f;
-    for (int i = 0; i < B - 1; i++) { fa[u][i + 1] = fa[fa[u][i]][i]; }
-    for (int v: e[u]) if (v != f) {
-      initLca(v, u); } }
-  void init() {
-    repa (u,n) if (d[u] == 0) { initLca(u,0); } }
-  int up(int u, int d) {
-    for (int i = 0; i < B; i++) { if ((d >> i) & 1) { u = fa[u][i]; } }
-    return u; }
-  int lca(int u, int v) {
-    if (d[u] < d[v]) { int tmp = u; u = v; v = tmp; }
-    u = up(u, d[u] - d[v]);
-    for (int i = B - 1; i >= 0; i--) {
-      if (fa[u][i] != fa[v][i]) { u = fa[u][i]; v = fa[v][i]; } }
-    return u == v ? u : fa[u][0]; }
-  int dis(int u, int v) {
-    if (!query(u,v)) { return -1; }
-    int l = lca(u,v); return d[u] + d[v] - 2*d[l]; }
+    Node p[N];
+    void split(int u, int v) {
+      p[u].makeRoot(); p[v].access(); p[v].splay(); }
+    void link(int u, int v) {
+      p[u].makeRoot(); p[u].f = &p[v]; }
+    void cut(int u, int v) {
+      split(u,v);
+      p[v].s[0] = p[u].f = nullptr; p[v].pushUp(); }
+    bool isUni(int u, int v) {
+      return p[u].findRoot() == p[v].findRoot(); }
+  } T;
+
+  std::queue<int> free_node;
+  TreeLCT () { 
+    constexpr int free_begin = 100001;
+    constexpr int free_end = 200000;
+    repi (i,free_begin,free_end) free_node.push(i); }
+  inline int newNode() { 
+    int res = free_node.front(); free_node.pop(); return res; }
+  inline void deleteNode(int u) {
+    T.p[u] = LCT_impl::Node(); free_node.push(u); }
+
+  int relay = -1;
+  virtual void addEdge(int u, int v) override final { 
+    slog("adding edge (%d,%d)\n", u,v);
+    relay = newNode(); T.p[relay].wei = 1; T.p[relay].val = 1;
+    T.link(u,relay); T.link(relay,v); }
+  void cutEdge(int u, int v, int relay) {
+    T.cut(u,relay); T.cut(relay,v); deleteNode(relay); }
+  struct trace_type { int u,v, relay; enum Type { link, add } type; };
+  int query(std::vector< std::pair<int,int> >extra_edge, int s, int t) {
+    std::stack<trace_type> trace;
+    for (auto pos: extra_edge) {
+      trace_type tp;
+      int u = pos.first, v = pos.second; tp.u=u; tp.v=v;
+      if (T.isUni(u,v)) {
+        tp.type = trace_type::add; 
+        T.split(u,v); T.p[v].pushAdd(0); }
+      else {
+        tp.type = trace_type::link;
+        addEdge(u,v); tp.relay = relay; }
+      trace.push(tp); }
+    int res = -1;
+    if (T.isUni(s,t)) { T.split(s,t); res = T.p[t].val_sum; }
+    while (!trace.empty()) {
+      auto tp = trace.top(); trace.pop(); int u=tp.u, v=tp.v;
+      if (tp.type == trace_type::add) {
+        T.split(u,v); T.p[v].pushAdd(1); }
+      else { cutEdge(u,v, tp.relay); } }
+    return res; }
 };
 
 GraphTcc Go;
-TreeS Gs;
+TreeLCT Gs;
 int n, m, q;
 
 void preInit() { } void init() {
@@ -100,11 +175,13 @@ void preInit() { } void init() {
   rep (i,m) { int u=sc.n(), v=sc.n(); Go.addEdge(u,v); }
   Go.solve(Gs);
 } void solve() {
-  Gs.init();
   q = sc.n();
   while (q--) {
     int s=sc.n(), t=sc.n(), k=sc.n();
     s = Go.bel[s]; t = Go.bel[t];
-    rep (i,k) { sc.n(); sc.n(); }
-    printf("%d\n", Gs.dis(s,t)); }
+    slog("answering query (s=%d,t=%d,k=%d)\n",s,t,k);
+    std::vector< std::pair<int,int> > extra_edge;
+    rep (i,k) { int u=sc.n(), v=sc.n(); 
+      extra_edge.push_back( std::make_pair(Go.bel[u],Go.bel[v]) ); }
+    printf("%d\n", Gs.query(extra_edge, s, t)); }
 }
