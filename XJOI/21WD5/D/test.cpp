9576e798e70eca7f8c56b1b73419f89f7536d15c
coderoj <coderoj@outlook.com>
2021-02-16 21:19:59 +0800

    upd(XJ 21WD5 D)

---
+++ XJOI/21WD5/D/test.cpp
@@ -0,0 +1,17 @@
+#include "./main.cpp"
+#include <iostream>
+
+int main() {
+{
+  EnergyGameOnGrid G;
+  std::cout << 
+  G.isReachable(
+
+#include INPUT
+
+  ) <<
+  std::endl;
+}
+
+  return 0;
+};
