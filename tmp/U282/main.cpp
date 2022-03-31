7e7b88571e55d4dafdc40dc54c066e1e107c6acc
CoderOJ <CoderOJ@outlook.com>
2022-03-31 13:37:25 +0800

    add(UOJ 282)

---
+++ tmp/U282/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+  int t;
+  std::cin >> t;
+  for (int i = 0; i < t; i++)
+  {
+    int n;
+    std::cin >> n;
+    puts(n > 3 ? "-1" : "1");
+  }
+  return 0;
+}
